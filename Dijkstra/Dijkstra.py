641f0289cfdb33c7eaca515b223aa0cfd041e593
Shin hyunsuk <wellshs@kaist.ac.kr>
2017-01-01 21:12:48 +0900

    dijstra primary queue없이 구현

--- Dijkstra/Dijkstra.py
+++ Dijkstra/Dijkstra.py
@@ -8,9 +8,32 @@ graph는 n*n matrix를 표현하기 위하여, n개의 element를 가지는 list
 graph[i][j]는 node i 에서 node j로 가는 길이 있으면 숫자가 있을 것이고 아닐 경우에는 math.inf가 들어간다.
 output : start node에서 부터 각 노드에 가는데 필요한 최소경로의 list
 '''
-def dijstra(graph = [[0,2,3,inf,1],[2, 0, 4, 5, inf],[3, 4, 0, 6, inf], [inf, 5, 6, 0, inf], [1, inf, inf, inf, inf]], start = 0):
-    n = len(graph)
-    res = [inf]*n
+def dijstra(graph = [[0,2,3,inf,inf],[inf, 0, 4, 5, inf],[inf, inf, 0, 6, inf], [inf, inf, inf, 0, inf], [1, inf, inf, inf, 0]], start = 0):
+    n = len(graph) #n은 node의 갯수
+    res = [inf]*n #result list 초깃값은 infinite로 지정
+    res[start] = 0
+    Q = []
+    def get_min(Q, res):
+        temp = inf
+        ret = None
+        for node in Q:
+            if temp>res[node]:
+                temp = res[node]
+                ret = node
+        if ret:
+            Q.remove(ret)
+        return ret
+    for i in range(n):
+        if i!=start:
+            Q.append(i)
+    while True:
+        for i in range(n):
+            if i in Q and graph[start][i]!=inf:
+                if res[i]>res[start]+graph[start][i]:
+                    res[i] = res[start]+graph[start][i]
+        start = get_min(Q, res)
+        if len(Q)==0 or start==None:
+            break
     return res
 
 print(dijstra())
